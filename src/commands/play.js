2c508226bfe65f1d678d405b955149807aa188a6
W4LX <crocraxkerw@gmail.com>
2023-10-21 18:36:01 -0500

    Update command play

--- src/commands/play.js
+++ src/commands/play.js
@@ -1,4 +1,5 @@
 import ytdl from "@distube/ytdl-core";
+import yts from "yt-search";
 import fs from "fs";
 import { tmpdir } from "os";
 import { join } from "path";
@@ -10,10 +11,16 @@ export default {
   run: async (socket, msg, args) => {
     try {
       // Obtiene la URL proporcionada como argumento
-      const url = args.join(" ");
+      const name = args.join(" ");
 
       // Verifica si se proporciona una URL
-      if (!url) return;
+      if (!name) {
+        socket.sendMessage(msg.messages[0]?.key.remoteJid, {
+          text: "Ingresa el nombre de la canción.",
+        });
+
+        return;
+      }
 
       // Crea un nombre de archivo temporal con marca de tiempo
       const tempFile = join(tmpdir(), Date.now() + ".mp3");
@@ -22,9 +29,22 @@ export default {
         react: { text: "⏳", key: msg.messages[0]?.key },
       });
 
+      const video = (await yts(name)).all.find((i) => i.type === "video");
+
+      if (!video) {
+        socket.sendMessage(msg.messages[0]?.key.remoteJid, {
+          text: "Sin resultados.",
+        });
+
+        return;
+      }
+
       // Descarga el audio de la URL de YouTube y guarda en el archivo temporal
       await new Promise((resolve, reject) => {
-        const res = ytdl(url, { filter: "audioonly", quality: "highestaudio" });
+        const res = ytdl(`https://youtu.be/${video.videoId}`, {
+          filter: "audioonly",
+          quality: "highestaudio",
+        });
 
         // Crea un flujo de escritura para guardar el archivo de audio
         res.pipe(fs.createWriteStream(tempFile));
@@ -53,7 +73,7 @@ export default {
       console.error(error);
 
       socket.sendMessage(msg.messages[0]?.key.remoteJid, {
-        text: "Url inválida.",
+        text: "¡Ups! Sucedió un error.",
       });
     }
   },
