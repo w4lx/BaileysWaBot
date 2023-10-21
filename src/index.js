293c8e739eb3d6e6b8050d2990e758cb9ef2443b
W4LX <crocraxkerw@gmail.com>
2023-10-21 14:17:31 -0500

    changess

--- src/index.js
+++ src/index.js
@@ -45,3 +45,12 @@ connectToWhatsApp().catch((err) => console.error(err));
 
 // Exporta la función para conectar a WhatsApp para su uso en otros módulos
 export { connectToWhatsApp };
+
+// Para que el bot no se apague
+process.on("uncaughtException", (error) => console.error(error));
+
+process.on("uncaughtExceptionMonitor", (error) => console.error(error));
+
+process.on("unhandledRejection", (error) => console.error(error));
+
+process.stdin.resume();
